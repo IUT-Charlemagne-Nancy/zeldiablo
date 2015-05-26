2c930572ae089522557a73570edac04f877fbf67
Benjamin Denys <benjamin.denys7@etu.univ-lorraine.fr>
2015-05-26 22:17:44 +0200

    "Correction des classes de Quentin"

--- src/personnage/Personnage.java
+++ src/personnage/Personnage.java
@@ -1,13 +1,15 @@
 package personnage;
 
-public class Personnage {
-	protected int posX,poxY,attaque,portee,vie;
-	public Heros(int x,int y,int attack,int porte,int pv) {
+public abstract class Personnage {
+	protected int posX,posY,attaque,portee,vie;
+	private boolean mort;
+	public  Personnage(int x,int y,int attack,int porte,int pv) {
 		this.posX=x;
 		this.posY=y;
 		this.attaque=attack;
 		this.portee=porte;
 		this.vie=pv;
+		this.mort = false;
 	}
 
 	public abstract void seDeplacer(String  choix);
@@ -23,7 +25,40 @@ public class Personnage {
 		}
 	}
 
-	@Override
+	public int getPosX() {
+		return posX;
+	}
+
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+
+	public int getPosY() {
+		return posY;
+	}
+
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
+
+	public int getPortee() {
+		return portee;
+	}
+
+	public void setPortee(int portee) {
+		this.portee = portee;
+	}
+
+	private boolean etreAPortee(Personnage victime) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	private boolean etreMort() {
+		// TODO Auto-generated method stub
+		return mort;
+	}
+
 	public void subirDegat(int x) {
 		if (!this.etreMort()){
 			this.vie=this.vie-x;
