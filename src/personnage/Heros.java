9d31a7297ce9d6a30c8b1472c137ab5aca7c7f6a
Benjamin Denys <benjamin.denys7@etu.univ-lorraine.fr>
2015-05-26 20:00:00 +0200

    "Ajout de la limitation des déplacements du personnage"

--- src/personnage/Heros.java
+++ src/personnage/Heros.java
@@ -22,6 +22,18 @@ public class Heros extends Personnage{
 		if (choix == "bas"){
 			this.posY = this.posY-10;
 		}
+		if (this.posX<0){
+			this.posX = 0;
+		}
+		if (this.posX<10){
+			this.posX = 10;
+		}
+		if (this.posY<0){
+			this.posY = 0;
+		}
+		if (this.posY<10){
+			this.posY = 10;
+		}
 		
 	}
 
