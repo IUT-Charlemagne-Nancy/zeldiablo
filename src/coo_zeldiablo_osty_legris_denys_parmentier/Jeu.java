a2acbdf17177104c6eca8a6d9a39c179da4ca189
Corentin Legris <corentin.legris9@etu.univ-lorraine.fr>
2015-05-26 17:41:03 +0200

    Ajout du diagramme de classe

--- src/coo_zeldiablo_osty_legris_denys_parmentier/Jeu.java
+++ src/coo_zeldiablo_osty_legris_denys_parmentier/Jeu.java
@@ -1,5 +1,25 @@
 package coo_zeldiablo_osty_legris_denys_parmentier;
 
+import java.util.Scanner;
+
 public class Jeu {
 
+	public static void main(String[]args){
+		System.out.println("Bonjour bienvenue dans Zeldiablo");
+		String nom="";
+		Scanner sc=new Scanner(System.in);
+		boolean valide=false;
+		while (valide==false){
+			System.out.println("Veuiller choisir un nom de personnage :");
+			nom=sc.nextLine();
+			System.out.println("Vous vous appeler donc : "+nom +"v/f" );
+			String valider=sc.nextLine();
+			if(valider.equals("v")){
+				valide=true;
+			}
+			
+		}
+	
+		
+	}
 }
