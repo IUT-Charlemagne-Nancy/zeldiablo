173653dc3ec531ca4f67f811ff6e86b5acdb1889
quentin Parmentier <quentin.prm@gmail.com>
2015-05-26 20:25:57 +0000

    Personnage.java edited online with Bitbucket

--- src/personnage/Personnage.java
+++ src/personnage/Personnage.java
@@ -2,14 +2,12 @@ package personnage;
 
 public abstract class Personnage {
 	protected int posX,posY,attaque,portee,vie;
-	private boolean mort;
-	public  Personnage(int x,int y,int attack,int porte,int pv) {
+	public Personnage(int x,int y,int attack,int porte,int pv) {
 		this.posX=x;
 		this.posY=y;
 		this.attaque=attack;
 		this.portee=porte;
 		this.vie=pv;
-		this.mort = false;
 	}
 
 	public abstract void seDeplacer(String  choix);
@@ -25,47 +23,46 @@ public abstract class Personnage {
 		}
 	}
 
-	public int getPosX() {
-		return posX;
-	}
-
-	public void setPosX(int posX) {
-		this.posX = posX;
-	}
-
-	public int getPosY() {
-		return posY;
-	}
-
-	public void setPosY(int posY) {
-		this.posY = posY;
-	}
-
-	public int getPortee() {
-		return portee;
-	}
-
-	public void setPortee(int portee) {
-		this.portee = portee;
-	}
-
-	private boolean etreAPortee(Personnage victime) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	private boolean etreMort() {
-		// TODO Auto-generated method stub
-		return mort;
-	}
-
 	public void subirDegat(int x) {
 		if (!this.etreMort()){
+			if(x<0){
+				x=0;
+			}
 			this.vie=this.vie-x;
 			if (this.vie<0){
 				this.vie=0;
 			}
 		}
 		
+	}
+	public boolean etreMort(){
+		boolean res=false;
+		if(this.vie<=0){
+			res=true;
+		}
+		return res;
+	}
+	public boolean etreAPortee(Personnage perso){
+		boolean res=false;
+		if(Math.abs(this.posX-perso.posX)<=portee&&Math.abs(this.posY-perso.posY)<=portee){
+			res=true;
+		}
+		return res;
+	}
+	public int getPosX(){
+		return this.posX;
+	}
+	public int getPosY(){
+		return this.posY;
+	}
+	public int getPortee(){
+		return this.portee;
+		
+	}
+	public int getDegat(){
+		return this.attaque;
+	}
+	public int getVie(){
+		return this.vie;
 	}
 }
