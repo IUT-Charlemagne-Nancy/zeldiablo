51c41c8f975596aba5eee6bb7fe412ad5b6b5ac9
Corentin Legris <corentin.legris9@etu.univ-lorraine.fr>
2015-05-26 19:53:08 +0200

    Je t'envoie tout Ben

---
+++ src/personnage/Jeu.java
@@ -0,0 +1,9 @@
+package personnage;
+
+public class Jeu {
+
+	public static void main(String[]args){
+		Labyrinthe a = new Labyrinthe();
+		
+	}
+}
---
+++ src/personnage/Labyrinthe.java
@@ -0,0 +1,23 @@
+package personnage;
+
+public class Labyrinthe {
+
+	private int tailleX;
+    private int tailleY;
+
+		
+	    public Labyrinthe(){
+		this.tailleX=10;
+		this.tailleY=10;
+	    }
+	    
+		public int getX() {
+			return this.tailleX;
+			
+		}
+		
+		public  int getY() {
+			return this.tailleY;
+		}
+	
+}
