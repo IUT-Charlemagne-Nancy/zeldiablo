ba0ba1b3eaefeae69d4545efded16c9956986882
Benjamin Denys <benjamin.denys7@etu.univ-lorraine.fr>
2015-06-09 15:58:58 +0200

    "Finalisation de la classe objet et Talisman"

--- src/Objet/Objet.java
+++ src/Objet/Objet.java
@@ -8,17 +8,20 @@ public abstract class Objet {
 	private String nomObjet;
 	private boolean estPris;
 	protected MonJeu jeu;
-
+	private int posX;
+	private int posY;
 	
 	/**
 	 * Constructeur d'un objet;
 	 * @param nom
 	 * @param pris
 	 */
-	public Objet(String nom,MonJeu j) {
+	public Objet(String nom,MonJeu j,int x,int y) {
 		this.nomObjet = nom;
 		this.jeu = j;
 		this.estPris = false;
+		this.posX = x;
+		this.posY = y;
 		}
 	
 	/**
@@ -29,6 +32,38 @@ public abstract class Objet {
 		this.appliquerEffet(heros);
 	}
 	
+	public String getNomObjet() {
+		return nomObjet;
+	}
+
+	public void setNomObjet(String nomObjet) {
+		this.nomObjet = nomObjet;
+	}
+
+	public boolean isEstPris() {
+		return estPris;
+	}
+
+	public void setEstPris(boolean estPris) {
+		this.estPris = estPris;
+	}
+
+	public int getPosX() {
+		return posX;
+	}
+
+	public void setPosX(int posX) {
+		this.posX = posX;
+	}
+
+	public int getPosY() {
+		return posY;
+	}
+
+	public void setPosY(int posY) {
+		this.posY = posY;
+	}
+
 	public abstract void appliquerEffet(Heros heros);
 
 }
--- src/Objet/Talisman.java
+++ src/Objet/Talisman.java
@@ -6,12 +6,14 @@ import jeu.MonJeu;
 public class Talisman extends Objet{
 
 	public Talisman(MonJeu j) {
-		super("Talisman magique",j);
+		super("Talisman magique",j,14,9);
 	}
 
 	@Override
-	public void appliquerEffet(Heros hero) {
-		hero.setVie(heros.);
+	public void appliquerEffet(Heros heros) {
+		heros.setVie(heros.getVie()*2+100);
+		heros.setAttaque(heros.getAttaque()*2);
+		heros.setPortee(heros.getPortee()*2);
 		
 	}
 
