2b946ca9703cf161fc11620cf5517d6b5333c309
Thomas Osty <thomas.osty@wanadoo.fr>
2015-06-09 17:19:14 +0200

    finito subito presto

--- src/test/TestTalisman.java
+++ src/test/TestTalisman.java
@@ -10,7 +10,7 @@ public class TestTalisman {
 	@org.junit.Test
 	public void test_Constructeur_Talisman(){
 		MonJeu j = new MonJeu();
-		Talisman t = new Talisman(j);
+		Talisman t = new Talisman();
 		assertEquals("le talisman doit etre en position x = 14",14,t.getPosX());
 		assertEquals("le talisman doit etre en position y = 9", 9, t.getPosY());
 	}
@@ -18,7 +18,7 @@ public class TestTalisman {
 	public void test_Effet_Talisman(){
 		MonJeu j = new MonJeu();
 		Heros h = new Heros(j);
-		Talisman t = new Talisman(j);
+		Talisman t = new Talisman();
 		t.appliquerEffet(h);
 		assertEquals("Le heros doit avoir 300pdv", 300, h.getVie());
 		assertEquals("Le heros doit avoir 10 d'attaque", 10, h.getAttaque());
@@ -29,19 +29,12 @@ public class TestTalisman {
 		MonJeu j = new MonJeu();
 		Heros h = new Heros(j);
 		h.subirDegat(50);
-		Talisman t = new Talisman(j);
+		Talisman t = new Talisman();
 		t.appliquerEffet(h);
 		assertEquals("Le heros doit avoir 200pdv", 200, h.getVie());
 		assertEquals("Le heros doit avoir 10 d'attaque", 10, h.getAttaque());
 		assertEquals("Le heros doit avoir 2 de portee", 2, h.getPortee());
 	}
-	@org.junit.Test
-	public void test_prendre_Talisman(){
-		MonJeu j = new MonJeu();
-		Heros h = new Heros(j);
-		Talisman t = new Talisman(j);
-		t.etrePris(h);
-		assertEquals("l'objet doit etre pris", true, t.isEstPris());
-	}
+
 	
 }
