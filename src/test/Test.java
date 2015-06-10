308f2e83531ce243e672b4dd0239b9cbe093276f
Thomas Osty <thomas.osty@wanadoo.fr>
2015-06-10 17:23:40 +0200

    Javadoc test

--- src/test/Test.java
+++ src/test/Test.java
@@ -6,31 +6,45 @@ import jeu.Mur;
 
 
 public class Test {
-	
+	/**
+	 * Test qui vérifie le constructeur de MonJeu
+	 */
 	@org.junit.Test
-	public void test_Jeu_Constructeur(){
+	public void test_MonJeu_Constructeur(){
 		MonJeu j = new MonJeu(1);
 		assertEquals("Le heros doit se trouver en x = 1",1,j.getPj().getPosX());
 		assertEquals("Le heros doit se trouver en y = 1",1,j.getPj().getPosY());
 	}
+	/**
+	 * Test qui vérifie le constructeur de Mur
+	 */
 	@org.junit.Test
 	public void test_Mur_Constructeur(){
 		Mur m = new Mur (0,0);
 		assertEquals("Le mur doit se trouver en x = 0",0,m.getPosX());
 		assertEquals("Le mur doit se trouver en y = 0",0,m.getPosY());
 	}
+	/**
+	 * Test qui vérifie qu'une case est occupée
+	 */
 	@org.junit.Test
-	public void test_Case_Occupee_Constructeur(){
+	public void test_Case_Occupee(){
 		MonJeu j = new MonJeu(1);
 		boolean res = j.caseOccuper(1,1);
 		assertEquals("la case doit être occupée", true, res);
 	}
+	/**
+	 * Test qui vérifie que le jeu se termine à la mort du personnage
+	 */
 	@org.junit.Test
 	public void test_Fin_Jeu_Perdu(){
 		MonJeu j = new MonJeu(1);
 		j.getPj().subirDegat(100);  
 		assertEquals("le jeu doit etre fini", true, j.etreFini());
 	}
+	/**
+	 * Test qui vérifie que le jeu se termine quand le heros a ramené le talisman
+	 */
 	@org.junit.Test
 	public void test_Fin_Jeu_Gagne(){
 		MonJeu j = new MonJeu(1);
