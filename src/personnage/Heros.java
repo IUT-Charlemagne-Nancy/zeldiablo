0a823420ac311b99072a8c25577ea763fccffc25
Benjamin Denys <benjamin.denys7@etu.univ-lorraine.fr>
2015-06-10 17:35:45 +0200

    "Modifiaction de la méthode de gestion des objets"

--- src/personnage/Heros.java
+++ src/personnage/Heros.java
@@ -9,12 +9,10 @@ import moteurJeu.Commande;
 public class Heros extends Personnage{
 
 	private boolean avoirTalisman;
-	private ArrayList<Objet> inventaire;
 	private final static int MAX_INVENTAIRE=10;
 	public Heros(MonJeu j) {
 			super(1,1,5,1,100,j);
 			avoirTalisman=false;
-			inventaire=new ArrayList<Objet>();
 	}
 	public String toString(){
 		return("vie: "+this.vie+" portee: "+this.portee+" Degats: "+this.attaque+" le personnage est en position: x:"+this.posX+" y: "+this.posY);
@@ -26,24 +24,14 @@ public class Heros extends Personnage{
 	}
 	public void prendre(Commande c){
 		
-		if (!this.etreMort() && inventaire.size()<MAX_INVENTAIRE){
+		if (!this.etreMort()){
 			jeu.donnerObjet(this);
 				
 		}
 	}
-	public void ajouterObjet(Objet objet) {
-		inventaire.add(objet);
-		
-	}
 	public void avoirPrisTalisman() {
 		avoirTalisman=true;
 		
-	}
-	public void consommerObjet(int i){
-		if(i<=inventaire.size()){
-		inventaire.get(i).appliquerEffet(this);
-		inventaire.remove(i);
-		}
 	}
 	public boolean avoirTalisman(){
 		return avoirTalisman;
