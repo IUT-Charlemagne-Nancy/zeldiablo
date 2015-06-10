73b4d2bdd66004c9285b989c69208d7d2ddb2650
Benjamin Denys <benjamin.denys7@etu.univ-lorraine.fr>
2015-06-10 16:09:44 +0200

    "Ajout de la poursuite par l'IA"

--- src/jeu/MonJeu.java
+++ src/jeu/MonJeu.java
@@ -83,13 +83,35 @@ public class MonJeu implements Jeu {
 	public void evoluer(Commande commande) {
 		this.getPj().seDeplacer(commande);
 		for (int i = 0 ; i<this.monstres.size();i++){
-			Commande c = new Commande((int)Math.round(Math.random()*8));
+			Commande c = new Commande();
 			if (this.etreAPorteeHeros(this.monstres.get(i))){
 				c.attaque = true;
 				monstres.get(i).attaquer(c);
 			}
-			monstres.get(i).seDeplacer(c);
-			
+			if (this.sentirPresence(this.monstres.get(i))){
+				int x = this.monstres.get(i).getPosX()-this.getPj().getPosX();
+				int y = this.monstres.get(i).getPosY()-this.getPj().getPosY();
+				if (x<0){
+					c.droite=true;
+					monstres.get(i).seDeplacer(c);
+				}
+				if (x>0){
+					c.gauche=true;
+					monstres.get(i).seDeplacer(c);
+				}
+				if(y<0){
+					c.bas=true;
+					monstres.get(i).seDeplacer(c);
+				}
+				if(y>0){
+					c.haut=true;
+					monstres.get(i).seDeplacer(c);
+				}
+			}
+			else{
+				Commande c2 = new Commande((int)Math.round(Math.random()*8));
+				monstres.get(i).seDeplacer(c2);
+			}			
 		}
 		if (commande.attaque == true){
 		this.getPj().attaquer(commande);
@@ -150,6 +172,15 @@ public class MonJeu implements Jeu {
 			return false;
 		}
 	}
+	
+	public boolean sentirPresence(Personnage p){
+		if ((int)Math.abs(p.getPosX()-this.getPj().getPosX())<=5 && (int) Math.abs(p.getPosY()-this.getPj().getPosY()) <= 5){
+			return true;
+		}
+		else{
+			return false;
+		}
+	}
 	/**
 	 * méthode qui sert a ajouter un monstre
 	 * @param m
@@ -166,7 +197,6 @@ public class MonJeu implements Jeu {
 		return this.monstres.get(i);
 	}
 	public void donnerObjet(Heros heros) {
-		boolean res=false;
 		for(int i=0;i<objets.size();i++){
 			if(objets.get(i).getPosX()==heros.getPosX() && objets.get(i).getPosY()==heros.getPosY() ){
 				heros.ajouterObjet(objets.get(i));
